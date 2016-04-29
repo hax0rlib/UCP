46053aed912d22163e1cbcd71e3e77667cfa055f
André Rodriguês <andretutors0@gmail.com>
2016-04-29 13:18:16 -0300

    Added files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+class DB
+{
+  private $pdo = null;
+  private $dsn = null;
+
+  public function __construct() {
+    $this->pdo = $pdo;
+    $this->dsn = $dsn;
+  }
+
+  private function getConnection() {
+    $this->dsn = "mysql:host=localhost;dbname=rpg";
+    $this->pdo = new PDO($this->dsn, $user, $pass);
+  }
+}
+
+?>
