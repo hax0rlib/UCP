f5cb08bf76eb7c9f6f7c22c4545bece290246c76
André Rodriguês <andretutors0@gmail.com>
2016-04-29 13:47:30 -0300

    Update config.php

--- config.php
+++ config.php
@@ -3,15 +3,16 @@ class DB
 {
     private $pdo = null;
     private $dsn = null;
+
     public function __construct()
     {
         $this->pdo = $pdo;
         $this->dsn = $dsn;
     }
+
     private function getConnection()
     {
         $this->dsn = "mysql:host=localhost;dbname=rpg";
         $this->pdo = new PDO($this->dsn, $user, $pass);
     }
 }
-?>
